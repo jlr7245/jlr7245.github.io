40d95b3d6635428e13b6783fae9cea7924ef7e6d
J Silverstein <jess.pebble@gmail.com>
2016-12-11 20:05:43 -0500

    adding annotations

--- hangman_starter/src/game.js
+++ hangman_starter/src/game.js
@@ -1,30 +1,39 @@
 console.log('game.js is connected');
 
 class Game {
-  constructor() {
+  constructor() { /// here's your constructor function
     this.guesses = null;
     this.guessedLetters = null;
     this.currentWord = null;
-  //Put attributes here!
+
   }
-  //Go to town on those methods below!
-  startGame(theWord) {
+
+  startGame(theWord) { /// startgame method
     this.guesses = 8;
     this.guessedLetters = [];
-    this.currentWord = new Word(theWord);
+    this.currentWord = new Word(theWord); /// we don't want currentWord to just be `theWord` the string. we want a word object like we created in word.js.
   }
   
-  guess(theGuess) {
-    if (this.guessedLetters.indexOf(theGuess) == -1) {
-      this.guessedLetters.push(theGuess);
-      this.currentWord.try(theGuess) ? console.log(`${ theGuess } is in the phrase!`) : this.guesses--;
-    } else console.log(`${ theGuess } has already been guessed.`);
+  guess(theGuess) { //// this method takes an argument because you want to do stuff with whatever you're guessing. in the console, you'll call this with `game.guess('a');` to guess `a`... so you have to let the method know that it should expect to see something.
+    if (this.guessedLetters.indexOf(theGuess) == -1) { /// like on line 279 of the appAlmostComplete.js solution for the LOTR homework... if we're checking our `guessedLetters` array and expecting to see the index of the letter we're guessing, we're expecting a number, right? well, if `theGuess` isn't in our `guessedLetters` array, its index is -1.
+      this.guessedLetters.push(theGuess); /// pushing the index into the array
+      if (this.currentWord.try(theGuess) === true) { /// nesting an 'if' statement within another 'if' statement
+        console.log('Good guess!'); 
+      } else {
+        this.guesses--; /// otherwise decrease the number of guesses
+        console.log(`Oh no, ${ theGuess } was not in the phrase. You have ${ this.guesses} guesses left!`);
+      }
+    } else console.log(`You've already guessed ${ theGuess }!`); /// if the guessed letter is in our array...
   }
   
-  isOver() {
+  isOver() { ///// i ran out of steam. i'll finish annotating tomorrow
     let isItOver = false;
-    if (this.guesses === 0) isItOver = true;
-     else if (this.currentWord.isFound() === true) isItOver = true;
+    if (this.guesses === 0) {
+      isItOver = true;
+    }
+     else if (this.currentWord.isFound() === true) {
+       isItOver = true;
+     }
      return isItOver;
   }
   
@@ -37,3 +46,14 @@ class Game {
   }
   
 }
+
+
+
+
+//// HOW TO CHECK YOUR GAME IN THE CONSOLE
+//// ---- type in these lines one at a time and hit enter after each one ---
+//// 1. let game = new Game() //// don't need anything in the parintheses ... yet
+//// 2. game.startGame('aleph') //// here is where we start the game. we're accessing the `startGame` _method_ within the `game` _object_, and passing it the _argument_ `aleph`.
+//// (optional) 3. game.currentWord /// let's take a look at the Word object we created up on line 14. You'll notice it has an array of letters already. We wanted it to automatically have this array of letters, so that we dont have to run something like `game.currentWord.getLetters()` every time we start a game. see line 9 of word.js. bonus: try running `game.currentWord.getLetters()` in the console and then `game.currentWord`. you'll see that every word is doubled.
+//// 4. game.guess('a') /// let's guess the letter a! 
+//// 5. game.currentWord.render() /// let's see what our game looks like. you should be seeing `a _ _ _ _`...
--- hangman_starter/src/word.js
+++ hangman_starter/src/word.js
@@ -3,47 +3,51 @@
 console.log('word.js is connected');
 
 class Word {
-  constructor(value) {
-  this.value = value;
-  this.letters = [];
-  this.getLetters();
+  constructor(value) { /// accepts a word argument, so you can type `let Aleph = new Word('aleph')` in the console and it will create a Word object based on the string `Aleph`
+  this.value = value; //// I don't think this was in the checker but I decided I liked having the string accessible from within the object without having the rely on the word argument from the constructor function.... if this doesn't make sense to you don't worry about it
+  this.letters = []; /// our blank array 
+  this.getLetters(); /// okay, this is a little tricky. when you create the word object you want the getLetters function below to run automatically, otherwise there's going to be an intermediate step. see line 48 of game.js for more discussion
   }
-  new() {
+  new() { /// left this guy blank bc i didn't need it
   }
   
-  getLetters() {
-    let letterArray = this.value.split('');
-    let x;
-    for (let i = 0; i < letterArray.length; i++) {
-      x = new Letter(letterArray[i]);
-      this.letters.push(x);
+  getLetters() { /// here is our getLetters function.
+    let letterArray = this.value.split(''); //// because I had this.value defined in the constructor function, this is what it looked like for me. if you hadn't defined `this.value = value`, your code might have looked something like this:
+    //// getLetters(value) {
+      //// let letterArray = value.split('');
+    let x; 
+    for (let i = 0; i < letterArray.length; i++) { /// loop-de-doop
+      x = new Letter(letterArray[i]); /// we want to create a new Letter object for each character in our value string. so, we're referring back to the `Letter` class we made back in letter.js and using the syntax to create a new _object_ from the _class_.  this is what the instructions meant by "creating individual `Letters` objects for each letter in the given word".
+      this.letters.push(x); /// we gotta add the letter object to the `this.letters` array
     }
   }
   
   isFound() {
-    let foundChecker = true;
-    for (let i = 0; i < this.letters.length; i++) {
-      if (this.letters[i].hidden === true) foundChecker = false;
+    let foundChecker = true; /// as we have all discovered the _very_ hard way, you can't have a return inside a loop. we've got a loop in this method, so we gotta put the return outside the loop. easiest way to do that 
+    for (let i = 0; i < this.letters.length; i++) { /// here's a nifty loop!
+      if (this.letters[i].hidden === true) { /// so here we're checking each _object_ in our `this.letters` _array_ to see if the _value_ `hidden` is `true`. 
+        foundChecker = false;
+      } /// don't need an else statement bc we don't want to change our checker back to true if it's changed to false above. there only needs to be one hidden letter for `isFound()` to be false.
     }
-    return foundChecker;
+    return foundChecker; /// returning outside the loop
   }
   
-  try(attempt) {
-    let tryChecker = false;
+  try(attempt) { /// we have to be able to pass an argument into this method, otherwise it won't run on anything.
+    let tryChecker = false; /// see note about foundChecker above
     for (let i = 0; i < this.letters.length; i++) { /// sets the loop for the letters
-      if (this.letters[i].value == attempt) {
-        this.letters[i].show();
-        tryChecker = true;
-      }
+      if (this.letters[i].value == attempt) { /// ok, so! our `Letters` class was constructed with two paramaters: `value` and `hidden`. we're looping through all of the `Letter` _objects_ in our `this.letters` _array_ and seeing if any of them are equal to the argument we're passing...
+        this.letters[i].show(); //// ... and if they are, we're running the `show` method from letter.js line 10....
+        tryChecker = true; //// and setting our tryChecker to true, because we want the method to return `true` when it runs.
+      } /// we don't need an else here because we tryChecker was false in the beginning and if no `Letter` objects in the array `letters` have a value of whatever our attempt was, we want the result to be `false` anyway
     }
-    return tryChecker;
+    return tryChecker; //// here's our return, well outside the loop
   }
   
   render() {
-    let returnString = [];
-    for (let i = 0; i < this.letters.length; i++) {
-      returnString.push(this.letters[i].render());
+    let returnString = []; //// putting our return outside the loop once again... this time it's a blank array
+    for (let i = 0; i < this.letters.length; i++) { /// loop loop loop
+      returnString.push(this.letters[i].render()); /// running the `render` method from line 13 of letters.js and pushing it into our returnString array
     }
-    return returnString.join(' ');
+    return returnString.join(' '); /// running the `join` method on our returnString array
   }
 }
